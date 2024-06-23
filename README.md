854601ee346e71a47d75ce1f3d3b11d4c8484d09
HamzaB505 <hamzaboulaalaop@gmail.com>
2024-06-24 00:12:50 +0200

    Basic rag system

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # medical_ai_transcriber
+
+Transcribe and create fancy reports from audio input & have a RAG system to retrieve data if needed.
+
+Steps:
+- Build simple RAG system
+- Package it as a streamlit app
+- Figure out how to take audio input -> make it text -> and give it to an LLM to analyse it and extract info
+- Build a structure for a report (to be validated with Thomas co-founder)
+
+
+# ressources
+
+RAG tutorial: https://www.youtube.com/watch?v=2TJxpyO3ei4&ab_channel=pixegami
+
+Document used: https://www.sfrm-gemmsor.fr/file/medtool/webmedtool/gemmtool01/botm0181/pdf00001.pdf
+
---
+++ app/_create_database.py
@@ -0,0 +1,129 @@
+import os
+import shutil
+import argparse
+import openai
+from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from _get_embedding_func import get_embedding_function
+from tqdm import tqdm
+
+CHROMA_PATH = "./chroma"
+DATA_PATH = "./data"
+
+def main():
+
+    # Check if the database should be cleared (using the --clear flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Clearing Database")
+        clear_database()
+
+    # Create (or update) the data store.
+    documents = load_documents()
+    chunks = split_documents(documents)
+    embedding_function = get_embedding_function()
+    add_to_chroma(chunks, embedding_function)
+
+
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
+
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=500,
+        length_function=len,
+        add_start_index=True,
+        is_separator_regex=False,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks")
+    
+    return chunks
+
+def create_chunk_ids(chunks):
+
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # if the page ID is the same as the last one, increment the index
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+        # Calculate the chunk ID.
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
+
+def add_to_chroma(chunks: list[Document],
+                  embedding_function):
+
+    print('starting embedding')
+    db = Chroma(
+        persist_directory=CHROMA_PATH,
+        embedding_function=embedding_function
+    )
+    print("finished embedding")
+    chunks_with_ids = create_chunk_ids(chunks)
+
+    #Add or Update the documents
+    existing_items = db.get(include=[]) #IDs are always included by default
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+    
+    # only add documents that don't exist in the DB
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        for i, chunk in tqdm(enumerate(new_chunks)):
+                print(f"Adding document {i+1}/{len(new_chunks)}")
+                db.add_documents([chunk], ids=[new_chunk_ids[i]])
+        print("Persisting changes...")
+        db.persist()
+        print("Changes persisted.")
+    else:
+        print("✅ No new documents to add")
+
+def clear_database():
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+def save_to_chroma(chunks: list[Document]):
+    # clears out if the database exists
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+    # create a new embeddings DB from the documents
+    db = Chroma.from_documents(
+        documents=chunks,
+        embedding_function=get_embedding_function(),
+        persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f'Saved {len(chunks)} chunks to {CHROMA_PATH}')
+
+
+if __name__ == "__main__":
+    main()
